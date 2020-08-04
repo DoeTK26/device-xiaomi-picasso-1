e92ec49fbeb4c273edb211b559abe39f68954d9a
Mango <mango@gmail.com>
2020-08-04 19:54:01 +0300

    Revert "davinci: Enable AUDIO_FEATURE_ENABLE_TFA98XX_FEEDBACK"
    This reverts commit 562a9856ad40b2026a40f32dc6cd0f630c14cbb6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,25 +36,6 @@ TARGET_USES_64_BIT_BINDER := true
 # Display density
 TARGET_SCREEN_DENSITY := 440
 
-#Audio
-AUDIO_FEATURE_ENABLED_TFA98XX_FEEDBACK := true
-
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
-
-# Audio
-USE_XML_AUDIO_POLICY_CONF := 1
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := lito
-TARGET_NO_BOOTLOADER := true
-
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
-
 # Kernel
 BOARD_KERNEL_BASE := 0x0000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7 buildvariant=user
